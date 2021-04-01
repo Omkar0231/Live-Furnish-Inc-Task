809c0be940498f51137013f005b5791bdff8ab40
omkar0231 <omkar.thouta0231@gmail.com>
2021-04-01 11:43:16 +0530

    completed the question4

--- Question4.py
+++ Question4.py
@@ -1,10 +1,15 @@
 #Question 4
 import random
+mylist = [1,2,3,4,5,6,7,8,9,0]
 
-mylist = [1,2,3,4,5,6,7,8,9,10]
-random.shuffle(mylist)
-for ele in mylist:
-    print(ele)
-
-
-
+def pick_random_number(mylist):
+    size = len(mylist)
+    while size:
+        index = random.randrange(size)
+        elem = mylist[index]
+        mylist.append(mylist.pop(index))
+        size = size - 1
+        print(elem)
+        
+pick_random_number(mylist)
+print(mylist)
